848265ef69154e33eb9be55a834e19dec11ee9cf
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2023-07-05 12:32:34 -0600

    Deleted prints

--- services/zabbix/alerts_service.py
+++ services/zabbix/alerts_service.py
@@ -248,8 +248,6 @@ def change_status(problemid: int, estatus: str, current_user_id: int):
         excepcion = session.query(ExceptionModel).filter(
             ExceptionModel.problemrecord_id == problem_record.problemrecord_id).first()
         excepcion.deleted_at = datetime.now()
-        print(problem_record.estatus)
-        print(excepcion.deleted_at)
         session.commit()
         session.refresh(excepcion)
     match estatus:
