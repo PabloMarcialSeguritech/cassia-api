66235f7db0484c022da60d56ac993213578ee88c
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2023-09-01 13:14:20 -0600

    added id column on catalogs

--- services/cat_service.py
+++ services/cat_service.py
@@ -1,9 +1,10 @@
+import numpy as np
+import pandas as pd
 from utils.settings import Settings
 from utils.db import DB_Zabbix
 from models.cassia_roles import CassiaRole as CassiaRolModel
 from utils.traits import success_response
 settings = Settings()
-
 '''
     Servicio para la administracion de catalogos
 '''
@@ -13,5 +14,8 @@ def get_roles():
     db_zabbix = DB_Zabbix()
     session = db_zabbix.Session()
     roles = session.query(CassiaRolModel).all()
+    roles = pd.DataFrame(roles).replace(np.nan, "")
+    if len(roles) > 0:
+        roles["id"] = roles["rol_id"]
     session.close()
     return success_response(data=roles)
