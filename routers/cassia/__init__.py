4b8be3f11017c08e2054acb5a15edc8149da5a26
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2023-09-08 14:53:40 -0600

    Fixed subtype filter, added correlations service

--- routers/cassia/__init__.py
+++ routers/cassia/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 from .users_routers import users_router
 from .roles_router import roles_router
 from .configurations_router import configuration_router
+from .host_correlation_router import host_correlation_router
 cassia_router = APIRouter(prefix="/api/v1/cassia")
 
 cassia_router.include_router(users_router)
 cassia_router.include_router(roles_router)
 cassia_router.include_router(configuration_router)
+cassia_router.include_router(host_correlation_router)
--- services/cassia/configurations_service.py
+++ services/cassia/configurations_service.py
@@ -1,22 +1,10 @@
 from utils.settings import Settings
 import pandas as pd
-from utils.db import DB_Zabbix, DB_Prueba
-from sqlalchemy import text, or_
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import HTTPException
-from models.user_model import User
-from models.user_has_roles import UserHasRole
-from models.cassia_roles import CassiaRole
-from models.cassia_permissions import CassiaPermission
-from models.role_has_permissions import RoleHasPermission
-import schemas.exception_agency_schema as exception_agency_schema
-import schemas.exceptions_schema as exception_schema
-import schemas.cassia_role_schema as cassia_role_schema
+from utils.db import DB_Zabbix
+from sqlalchemy import text
 
 import numpy as np
 from utils.traits import success_response
-from models.cassia_config import CassiaConfig
 settings = Settings()
 
 
