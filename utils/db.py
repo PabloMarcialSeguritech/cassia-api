ba5708b746744e11aeb6ebc1cd293c347527256d
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2023-11-03 12:29:38 -0600

    Added named instance var, changed to Disponibilidad the attribute in reports

--- utils/db.py
+++ utils/db.py
@@ -86,9 +86,9 @@ class DB_C5():
     DB_C5_PASS = settings.db_c5_pass
     DB_C5_HOST = settings.db_c5_host
     DB_C5_PORT = settings.db_c5_port
-
-    connection_string = f"mssql+pymssql://{DB_C5_USER}:{DB_C5_PASS}@{DB_C5_HOST}:{DB_C5_PORT}/{DB_C5_NAME}"
-
+    DB_C5_INSTANCIA_NOMBRADA = settings.db_c5_instancia_nombrada
+    connection_string = f"mssql+pymssql://{DB_C5_USER}:{DB_C5_PASS}@{DB_C5_HOST}:{DB_C5_PORT}/{DB_C5_NAME}" if not DB_C5_INSTANCIA_NOMBRADA else f"mssql+pymssql://{DB_C5_USER}:{DB_C5_PASS}@{DB_C5_HOST}/{DB_C5_NAME}"
+    print(connection_string)
     engine = create_engine(connection_string, echo=False,
                            poolclass=NullPool, pool_recycle=1800, pool_pre_ping=True)
 
