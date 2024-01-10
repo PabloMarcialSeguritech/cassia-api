bf4814886dbfe41e8e0651050a9b68b5ce42a648
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2024-01-10 12:58:02 -0600

    Added shcheduler to send notifications via slack

---
+++ models/cassia_slack_user_notification.py
@@ -0,0 +1,16 @@
+from utils.db import DB_Zabbix
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Text
+from datetime import datetime
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import mapped_column
+from models.problem_record import ProblemRecord
+from typing import List
+
+
+class CassiaSlackUserNotification(DB_Zabbix.Base):
+    __tablename__ = "cassia_slack_user_notifications"
+    slack_user_notification_id = Column(
+        Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, nullable=False)
+    last_date = Column(DateTime, nullable=False)
