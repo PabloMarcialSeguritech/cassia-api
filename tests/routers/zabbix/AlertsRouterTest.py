c5cf92e3aa7a80520c35e096abbdecce6a7af4e1
SergioMenaSeguritech <sergio.mena@seguritech.com>
2024-03-15 18:44:52 -0600

    ACKS - e subieron cambios de ajuste en la variable  de la condicional
    ACKS - e subieron cambios de ajuste en la variable  de la condicional

--- tests/routers/zabbix/AlertsRouterTest.py
+++ tests/routers/zabbix/AlertsRouterTest.py
@@ -18,18 +18,38 @@ def user_authentication_headers(username: str, password: str):
 
 class AlertsRouterTest(unittest.TestCase):
 
+    @unittest.skip("Omit test ack cassia")
     def test_acknowledge_cassia(self):
         print('> Entrando a test_acknowledge_cassia <')
         headers = user_authentication_headers("juan.marcial@seguritech.com", "12345678")
-        eventid = 674
+        eventid = 2
+        texto_esperado = "correctamente"
+        response_reboot = client.post(
+            f"/api/v1/zabbix/problems/acknowledge_/{eventid}",
+            headers=headers,
+            data={
+                "message": "Prueba CASSIA",
+                "close": 0,
+                "is_zabbix_event": 0
+            }
+        )
+        json_obj = response_reboot.json()
+        print("json_obj:", json_obj)
+        self.assertIn(texto_esperado, json_obj['message'])
+
+    @unittest.skip("Omit test ack zabbix")
+    def test_acknowledge_zabbix(self):
+        print('> Entrando a test_acknowledge_zabbix <')
+        headers = user_authentication_headers("juan.marcial@seguritech.com", "12345678")
+        eventid = 8998453
         texto_esperado = "correctamente"
         response_reboot = client.post(
             f"/api/v1/zabbix/problems/acknowledge_/{eventid}",
             headers=headers,
             data={
                 "message": "Mensaje de prueba",
-                "close": 1,
-                "is_zabbix_event": 0
+                "close": 0,
+                "is_zabbix_event": 1
             }
         )
         json_obj = response_reboot.json()
