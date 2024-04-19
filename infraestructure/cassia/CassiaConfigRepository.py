81f3cc9cb9663dad738c7232845e92c0634c703b
PabloMarcialSeguritech <juan.marcial@seguritech.com>
2024-04-19 11:56:46 -0600

    Actualizado endpoint de host unitario de descarga y de consulta a async

--- infraestructure/cassia/CassiaConfigRepository.py
+++ infraestructure/cassia/CassiaConfigRepository.py
@@ -57,11 +57,11 @@ async def get_city_catalog() -> pd.DataFrame:
         ).stored_name_city_catalog
         await db_model.start_connection()
         city_catalog_data = await db_model.run_stored_procedure(sp_get_city_catalog, ())
-        city_catalog_df = pd.DataFrame(city_catalog_df)
+        city_catalog_df = pd.DataFrame(city_catalog_data)
         return city_catalog_df
 
     except Exception as e:
-        print(f"Excepcion en get_config_value_by_name: {e}")
+        print(f"Excepcion en get_city_catalog: {e}")
         raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail=f"Excepcion en get_city_catalog: {e}")
     finally:
