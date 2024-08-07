7b3476157e043ea498a9bcd2e795ca62235e009d
Sergio Mena <137554750+SergioMenaSeguritech@users.noreply.github.com>
2024-08-07 16:09:13 -0600

    Uploaded refactor or reset pmi and refactor of query for object id .
    Uploaded refactor or reset pmi and refactor of query for object id .

--- routers/cassia/resets_router.py
+++ routers/cassia/resets_router.py
@@ -7,13 +7,13 @@ resets_router = APIRouter(prefix="/resets")
 
 
 @resets_router.post(
-    '/restart',
+    '/reset_pmi',
     tags=["Cassia - Resets"],
     status_code=status.HTTP_200_OK,
     summary="Restart a Reset",
     dependencies=[Depends(auth_service2.get_current_user_session)]
 )
-async def create_maintenance_async(objectId: str = ""):
+async def reset_pmi_async(affiliation: str = "", hostid: str = ""):
     reset_service = ResetServiceImpl()
-    result = await reset_service.restart_reset(objectId)
+    result = await reset_service.reset_pmi(affiliation, hostid)
     return result
--- tests/services/integration/ResetsServiceTest.py
+++ tests/services/integration/ResetsServiceTest.py
@@ -2,6 +2,7 @@ import unittest
 import asyncio
 from services.integration.reset_service_service_impl import ResetServiceImpl
 import json
+import pandas as pd
 
 class ResetsServiceTest(unittest.TestCase):
 
@@ -69,6 +70,7 @@ class ResetsServiceTest(unittest.TestCase):
 
         self.loop.run_until_complete(async_test())
 
+    @unittest.skip("Omit test_compare_and_add_or_update_reset")
     def test_compare_and_add_or_update_reset(self):
         print("> Entrando test_compare_and_add_or_update_reset <")
 
@@ -89,8 +91,92 @@ class ResetsServiceTest(unittest.TestCase):
 
         async def async_test():
             object_id = '64e6848929dd4b3df7a90f60'
-            resets = await reset_service.restart_reset(object_id)
-            response_dict = json.loads(resets.body)
-            print(response_dict)
+            hostid = '15098'
+            resets = await reset_service.restart_reset(object_id, hostid)
+            #response_dict = json.loads(resets.body)
+            #print(response_dict)
+            print(resets)
 
         self.loop.run_until_complete(async_test())
+
+    @unittest.skip("Omit test_restart_reset")
+    def test_get_devices_related(self):
+        print("> Entrando test_get_devices_related <")
+
+        reset_service = ResetServiceImpl()
+
+        async def async_test():
+
+            devices = await reset_service.getDispositivosRelacionadosCapa1('15098')
+            print(devices)
+
+        self.loop.run_until_complete(async_test())
+
+    @unittest.skip("Omit test_get_devices_pmi")
+    def test_get_devices_pmi(self):
+        print("> Entrando test_get_devices_pmi <")
+
+        reset_service = ResetServiceImpl()
+
+        async def async_test():
+
+            devices = await reset_service.get_devices_pmi('15098')
+            print(devices)
+
+        self.loop.run_until_complete(async_test())
+
+    @unittest.skip("Omit test_get_devices_pmi_status_connection")
+    def test_get_devices_pmi_status_connection(self):
+        print("> Entrando test_get_devices_pmi_status_connection <")
+
+        reset_service = ResetServiceImpl()
+
+        async def async_test():
+            data = {
+                'hostid': [15093, 15094, 15095, 15096, 15097, 15098, 21562],
+                'host': [
+                    'GUAN-CU23-CCTV-UNV-IPC2322EBR5-DUPZ-C-FIJA-FIJA',
+                    'GUAN-CU23-CCTV-UNV-IPC2322EBR5-DUPZ-C-FIJA-FIJA',
+                    'GUAN-CU23-CCTV-UNV-IPC2322EBR5-DUPZ-C-FIJA-FIJA',
+                    'GUAN-CU23-CCTV-UNV-IPC7622ER-X44U-PTZ-PTZ-GTO',
+                    'GUAN-CU23-CCTV-MOXA-EDS-408A-SWITCH-SWITCH-GTO',
+                    'GUAN-CU23-CCTV-CAMBIUM_NETWORKS-FORCE300-16-SUSCRIPTOR',
+                    'GUAN-RP_SAN MIGUEL-STD-CAMBIUM_NETWORK-EPMP_30'
+
+                ],
+                'alias': ['GTO-VVU-GUAN-023', 'GTO-VVU-GUAN-023', 'GTO-VVU-GUAN-023', 'GTO-VVU-GUAN-023',
+                          'GTO-VVU-GUAN-023', 'GTO-VVU-GUAN-023', 'GTO-STD-GUAN-TOSE002'],
+                'name': ['FIJA_1', 'FIJA_2', 'FIJA_3', 'PTZ', None, 'SUSCRIPTOR', 'AP4'],
+                'device_id': [4, 4, 4, 8, 13, 11, 2],
+                'ip': ['172.21.214.39', '172.21.214.40', '172.21.214.41', '172.21.214.38', '172.17.214.21',
+                       '172.17.214.20', '172.17.114.5'],
+                'b_interes': [0, 0, 0, 0, 1, 1, 1],
+                'b_ubicacion': [1, 1, 1, 1, 1, 1, 1],
+                'capa': [1, 1, 1, 1, 1, 1, 1]
+            }
+            devices_pmi_df = pd.DataFrame(data)
+            devices_df = await reset_service.get_devices_pmi_status_connection(devices_pmi_df)
+            print(devices_df)
+
+        self.loop.run_until_complete(async_test())
+
+
+    def test_reset_pmi_5_min_arriba(self):
+        print("> Entrando test_reset_pmi <")
+        '''
+            Prueba de 5 min y no response
+            PMI Arriba
+        '''
+
+        reset_service = ResetServiceImpl()
+
+        async def async_test():
+            affiliation = 'GTO-VVU-GUAN-023'
+            hostid = '15098'
+            devices_df = await reset_service.reset_pmi(affiliation, hostid)
+            print(devices_df)
+
+        self.loop.run_until_complete(async_test())
+
+
+
