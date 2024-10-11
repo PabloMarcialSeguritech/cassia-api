88da6d96c88b6771f88a77e9f4c3245226427e3e
SergioMenaSeguritech <137554750+SergioMenaSeguritech@users.noreply.github.com>
2024-10-11 16:55:22 -0600

    Adjust Query for get CASSIA host devices.
    Adjust Query for get CASSIA host devices.

--- infraestructure/db_queries_model.py
+++ infraestructure/db_queries_model.py
@@ -298,7 +298,9 @@ GROUP BY h.hostid, h.host, hi.ip, h.status, hi.dns, hi.useip, hi.port, h.descrip
 
         self.query_statement_update_cassia_group_name_and_type_id = None
 
-        self.query_statement_get_cassia_host_devices = "SELECT * FROM host_device"
+        self.query_statement_get_cassia_host_devices = """select hd.dispId, hd.name, hd.description, 
+            COUNT(hi.device_id) as host_count from host_device hd 
+            LEFT JOIN host_inventory hi ON hd.dispId = hi.device_id GROUP BY hd.dispId, hd.name"""
 
     def builder_query_statement_get_metrics_template(self, tech_id, alineacion_id):
         self.query_statement_get_metrics_template = f"""select * from metrics_template mt where device_id ='{tech_id}' and group_id ='{alineacion_id}'"""
