7cac35390df411354530000cde5c39a4f57cb6fd
SergioMenaSeguritech <137554750+SergioMenaSeguritech@users.noreply.github.com>
2024-10-22 12:02:56 -0600

    The way of groupping audit operations has been uploaded
    The way of groupping audit operations has been uploaded

--- routers/cassia/cassia_audit_router.py
+++ routers/cassia/cassia_audit_router.py
@@ -10,24 +10,6 @@ from datetime import datetime
 
 cassia_audit_router = APIRouter(prefix="/audit")
 
-'''@cassia_audit_router.get(
-    "/",
-    tags=["Cassia - Audit"],
-    status_code=status.HTTP_200_OK,
-    summary="Obtiene los audit logs",
-    dependencies=[Depends(auth_service2.get_current_user_session)]
-)
-async def get_audit_logs(
-    start_date: date = Query(..., description="Fecha de inicio en formato ISO (YYYY-MM-DD)", example="2024-01-01"),
-    end_date: date = Query(..., description="Fecha de fin en formato ISO (YYYY-MM-DD)", example="2024-12-31"),
-    user_email: Optional[str] = Query(None, description="Email del usuario"),
-    module_id: Optional[int] = Query(None, description="ID del módulo"),
-    page: int = Query(1, ge=1),
-    page_size: int = Query(10, le=4000),
-    db: DB = Depends(get_db)
-):
-
-    return await cassia_audit_service.get_audit_logs(start_date, end_date, user_email, module_id, page, page_size, db)'''
 
 @cassia_audit_router.get(
     "/",
@@ -49,7 +31,7 @@ async def get_audit_logs(
 
 @cassia_audit_router.post(
     "/export",
-    tags=["Cassia - Audit", "CASSIA Exports"],
+    tags=["Cassia - Audit"],
     status_code=status.HTTP_200_OK,
     summary="Exporta los registros de auditoria de CASSIA",
     dependencies=[Depends(auth_service2.get_current_user_session)]
@@ -59,7 +41,7 @@ async def export_audit_data(export_data: cassia_audit_schema.CassiaAuditExportSc
 
 @cassia_audit_router.post(
     "/",
-    tags=["Cassia - Audit", "CASSIA Create Log Audit"],
+    tags=["Cassia - Audit"],
     status_code=status.HTTP_200_OK,
     summary="Crea un registro de auditoria",
     dependencies=[Depends(auth_service2.get_current_user_session)]
